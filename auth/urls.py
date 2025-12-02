ac416c9c0f7bae81d8a03ac904726d5b521e6e5f
Marcos-felip <marcosfelip2002@gmail.com>
2025-12-02 15:17:12 -0300

    refactor: revertendo mudanças

--- auth/urls.py
+++ auth/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
-from .views import RegisterView, ChangePasswordView, CreateOrganizationView
+from .views import RegisterView, CreateOrganizationView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
-    path('change-password/', ChangePasswordView.as_view(), name='change_password'),
     path('create-organization/', CreateOrganizationView.as_view(), name='create_organization'),
 ]
