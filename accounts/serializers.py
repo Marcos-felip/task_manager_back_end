14a9d977facf81c38961baa146b7b92251d1234b
Marcos-felip <marcosfelip2002@gmail.com>
2025-12-04 11:59:51 -0300

    feat: adicionar campo 'id' ao serializer de membros da organização

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -26,6 +26,7 @@ class OrganizationMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = [
+            'id',
             'email',
             'first_name',
             'last_name',
